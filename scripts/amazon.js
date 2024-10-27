3322dd69b90a91ca0509c41565513e4dcf39f7be
shoaib-amazon-project <b-24080@student.usa.edu.pk>
2024-10-27 17:05:09 +0500

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,3 @@
-const products = [
-  {
-    iamge: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    iamge: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    iamge: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
-
 
 let productHTML = '';
 
@@ -36,7 +6,7 @@ products.forEach((product) => {
         <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="${product.iamge}">
+              src="${product.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
