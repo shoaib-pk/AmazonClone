e4e6ef506f533eb10ed97e30c430245e3bc2d1d5
shoaib-amazon-project <b-24080@student.usa.edu.pk>
2024-11-03 21:31:33 +0500

    Save data and genrate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,18 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart = JSON.parse(localStorage.getItem("cart"));
 
-if(!cart)
-{
-    cart = [
-     
-      ];
-      
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+      deliveryOptionId: '1'
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+      deliveryOptionId: '2'
+    },
+  ];
 }
 
 function saveToStorage() {
@@ -28,6 +35,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
+      deliveryOptionId:'1'
     });
     //console.log(cart);
   }
@@ -47,11 +55,9 @@ export function removeFromCart(productId) {
   saveToStorage();
 }
 
-export function updateQunatity(productId, newQuantity)
-{
-  cart.forEach((item)=>{
-    if(item.productId== productId)
-    {
+export function updateQunatity(productId, newQuantity) {
+  cart.forEach((item) => {
+    if (item.productId == productId) {
       item.quantity = newQuantity;
     }
   });
--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -2,3 +2,5 @@ export function fromatCurrency(priceCents)
 {
    return (priceCents/100).toFixed(2);
 }
+
+export default fromatCurrency;
